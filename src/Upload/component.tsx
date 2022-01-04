492fd86061fb8f376437b30eae01d0bf62a7e3ce
Charles-ShiZ <62864818+Charles-ShiZ@users.noreply.github.com>
2022-01-04 18:07:36 +0800

    添加上传psd重复时提示语

--- src/Upload/component.tsx
+++ src/Upload/component.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { UploadProps, UploadFile, EWidth } from "./types";
-import { psdResolve, fileToBase64, base64ToMd5} from "./utils";
+import { psdResolve, fileToBase64, base64ToMd5 } from "./utils";
 
 export default function Upload({
   type,
@@ -9,18 +9,19 @@ export default function Upload({
   multiple = true,
   style = {},
   size = "small",
-  value = [],
+  value = []
 }: UploadProps) {
   const width = EWidth[size];
   const uploadCompId = +new Date();
-  const [images, setImages] = useState<{ src: string, loaded: boolean }[]>([]);
+  const [images, setImages] = useState<{ src: string; loaded: boolean }[]>([]);
+
   useEffect(() => {
     if (value.length > images.length) {
-      const newImages = value.slice(images.length).map(uploadFile => ({
-        src:uploadFile.image,
-        loaded:false
+      const newImages = value.slice(images.length).map((uploadFile) => ({
+        src: uploadFile.image,
+        loaded: false
       }));
-      newImages.forEach(({src},index) => {
+      newImages.forEach(({ src }, index) => {
         const image = new Image();
         image.src = src;
         image.onload = () => {
@@ -30,33 +31,61 @@ export default function Upload({
       });
     }
   }, [value]);
+
   const getUploadFileList = async (files: FileList) => {
-    const uploadFileList = await Promise.all((() => {
-      const cacheUids = [];
-      const uploadFileList = Array.from(files).map(async (file) => {
-        const fileBase64 = await fileToBase64(file);
-        const uploadFile: UploadFile = {
-          uid: "", type, file, fileBase64, image: fileBase64,
-          name: file.name,
-          size: file.size,
-        };
-        if (type === "psd") {
-          const { uid, layers, layerNameList, colorMode, width, height, treeExport, image } = await psdResolve(file);
-          uploadFile.uid = uid;
-          uploadFile.image = image.base64;
-          uploadFile.psd = { layers, layerNameList, colorMode, width, height, treeExport };
-        } else {
-          uploadFile.uid = base64ToMd5(fileBase64);
-        }
-        if ([...value.map(v => v.uid), ...cacheUids].find(uid => uid === uploadFile.uid)) {
-          return null;
-        }
-        cacheUids.push(uploadFile.uid);
-        return uploadFile;
-      });
-      return uploadFileList;
-    })());
-    return uploadFileList.filter(i => i);
+    const uploadFileList = await Promise.all(
+      (() => {
+        const cacheUids = [];
+        const uploadFileList = Array.from(files).map(async (file) => {
+          const fileBase64 = await fileToBase64(file);
+          const uploadFile: UploadFile = {
+            uid: "",
+            type,
+            file,
+            fileBase64,
+            image: fileBase64,
+            name: file.name,
+            size: file.size
+          };
+          if (type === "psd") {
+            const {
+              uid,
+              layers,
+              layerNameList,
+              colorMode,
+              width,
+              height,
+              treeExport,
+              image
+            } = await psdResolve(file);
+            uploadFile.uid = uid;
+            uploadFile.image = image.base64;
+            uploadFile.psd = {
+              layers,
+              layerNameList,
+              colorMode,
+              width,
+              height,
+              treeExport
+            };
+          } else {
+            uploadFile.uid = base64ToMd5(fileBase64);
+          }
+          if (
+            [...value.map((v) => v.uid), ...cacheUids].find(
+              (uid) => uid === uploadFile.uid
+            )
+          ) {
+            alert("重复了");
+            return null;
+          }
+          cacheUids.push(uploadFile.uid);
+          return uploadFile;
+        });
+        return uploadFileList;
+      })()
+    );
+    return uploadFileList.filter((i) => i);
   };
   return (
     <form style={{ display: "inline-block" }}>
@@ -68,70 +97,74 @@ export default function Upload({
           accept={`.${type}`}
           value="" // 必须设置为空值，保证 input 不会缓存数据，导致无法连续导入相同文件
           style={{
-            display: "none",
+            display: "none"
           }}
           onChange={async (e) => {
             const { files } = e.target;
-            onChange([...value, ...await getUploadFileList(files)]);
+            onChange([...value, ...(await getUploadFileList(files))]);
           }}
         />
         <div style={style}>
           {children || (
             <div
               style={{
-                display: "flex",
+                display: "flex"
               }}
             >
-              {value.filter((_, i) => images[i]?.loaded).map((uploadFile, i) => (
-                <div
-                  key={uploadFile.uid}
-                  onClick={(e) => {
-                    e.preventDefault();
-                  }}
-                  style={{
-                    boxSizing: "border-box",
-                    height: width + "px",
-                    border: "1px solid #eeeeee",
-                    padding: "5px",
-                    borderRadius: "3px",
-                    display: "flex",
-                    position: "relative",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: "10px",
-                  }}
-                >
+              {value
+                .filter((_, i) => images[i]?.loaded)
+                .map((uploadFile, i) => (
                   <div
-                    style={{
-                      position: "absolute",
-                      bottom: "0px",
-                      right: "0px",
-                      width: "15px",
-                      height: "15px",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      color: "white",
-                      background: "rgb(1, 1, 1,0.4)",
-                    }}
+                    key={uploadFile.uid}
                     onClick={(e) => {
                       e.preventDefault();
-                      const newUploadFileList = value.filter((item, j) => i !== j);
-                      console.log(newUploadFileList);
-                      onChange(newUploadFileList);
+                    }}
+                    style={{
+                      boxSizing: "border-box",
+                      height: width + "px",
+                      border: "1px solid #eeeeee",
+                      padding: "5px",
+                      borderRadius: "3px",
+                      display: "flex",
+                      position: "relative",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginRight: "10px"
                     }}
                   >
-                    <svg width="100%" height="100%" viewBox="0 0 400 400">
-                      <path
-                        d="M 120 120 L 280 280 M 120 280 L 280 120"
-                        stroke="white"
-                        strokeWidth="20"
-                      />
-                    </svg>
+                    <div
+                      style={{
+                        position: "absolute",
+                        bottom: "0px",
+                        right: "0px",
+                        width: "15px",
+                        height: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "white",
+                        background: "rgb(1, 1, 1,0.4)"
+                      }}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        const newUploadFileList = value.filter(
+                          (item, j) => i !== j
+                        );
+                        console.log(newUploadFileList);
+                        onChange(newUploadFileList);
+                      }}
+                    >
+                      <svg width="100%" height="100%" viewBox="0 0 400 400">
+                        <path
+                          d="M 120 120 L 280 280 M 120 280 L 280 120"
+                          stroke="white"
+                          strokeWidth="20"
+                        />
+                      </svg>
+                    </div>
+                    <img src={uploadFile.image} height="100%" alt="" />
                   </div>
-                  <img src={uploadFile.image} height="100%" alt="" />
-                </div>
-              ))}
+                ))}
               <div
                 style={{
                   boxSizing: "border-box",
@@ -142,7 +175,7 @@ export default function Upload({
                   borderRadius: "3px",
                   display: "flex",
                   justifyContent: "center",
-                  alignItems: "center",
+                  alignItems: "center"
                 }}
               >
                 <svg width="100%" height="100%" viewBox="0 0 400 400">
